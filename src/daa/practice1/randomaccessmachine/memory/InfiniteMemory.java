b9dceaee7403c11ac3421f93a7c400289c2461da
Angel Igareta <alu0100967111@ull.edu.es>
2018-02-12 15:59:16 -0500

    Memory classes documented.

--- src/daa/practice1/randomaccessmachine/memory/InfiniteMemory.java
+++ src/daa/practice1/randomaccessmachine/memory/InfiniteMemory.java
@@ -1,28 +1,55 @@
-/**
- * 
- */
+/** File InfiniteMemory. */
 package daa.practice1.randomaccessmachine.memory;
 
+/** Import ArraList due the class InfiniteMemory uses it. */
 import java.util.ArrayList;
 
+
 /**
- * @author angel
- *
+ * InfiniteMemory is an abstract class that simulates an infinite ArrayList.
+ * The subclasses of InfiniteMemory must implement a getter and a setter of that
+ * ArrayList.
+ * @author Ángel Igareta
+ * @version 1.0
+ * @since 12-2-2018
  */
 public abstract class InfiniteMemory<T> {
 
-	ArrayList<T> memory;
-	Integer maxCapacity;
-
-	public InfiniteMemory() {
-		maxCapacity = new Double(Math.pow(10, 7)).intValue(); // TODO: Worth it?
-		memory = new ArrayList<T>(maxCapacity);
+	/** Generic ArrayList. */
+	protected ArrayList<T> memory;
+	/** Constant that defines the minimum value of the memory. */
+	private final int MAX_CAPACITY = new Double(Math.pow(10, 7)).intValue(); // TODO: Worth it?
+	
+	/**
+	 * Constructor of the InfiniteMemory class. It creates the instance of the
+	 * generic ArrayList assigning it a minimum size.
+	 */
+	protected InfiniteMemory() {
+		memory = new ArrayList<T>(MAX_CAPACITY);
 	}
 	
+	/**
+	 * Method that uses a hashCode to assign an index to an position in the memory.
+	 * It uses the module type.
+	 * @param index The cyclic index.
+	 * @return The hash position that matches the index passed by argument.
+	 */
 	protected int getPosition(int index) {
-		return index % maxCapacity;
+		return index % MAX_CAPACITY;
 	}
 	
+	/**
+	 * Abstract Method that subclasses must implement to specify how a data in the register
+	 * can be accessed.
+	 * @param index Index of the register.
+	 * @return Data at the memory index passed by argument.
+	 */
 	protected abstract T getRegisterAt(int index);
+	/**
+	 * Abstract Method that subclasses must implement to specify how a data in the register
+	 * can be accessed and changed.
+	 * @param index Index of the register.
+	 * @param data Data that has to be at the memory index passed by argument.
+	 */
 	protected abstract void setRegisterAt(int index, T data);
 }
--- src/daa/practice1/randomaccessmachine/memory/ProgramMemory.java
+++ src/daa/practice1/randomaccessmachine/memory/ProgramMemory.java
@@ -1,16 +1,25 @@
-/**
- * 
- */
+/** File ProgramMemory. */
 package daa.practice1.randomaccessmachine.memory;
 
+/** Import the necessary classes for FileReader and BufferedReader.*/
 import java.io.*;
 
+
 /**
- * @author angel
- *
+ * Class that contains an array of the Program Register that the Random Access Machine
+ * will use.
+ * @author Ángel Igareta
+ * @version 1.0
+ * @since 12-2-2018
  */
 public class ProgramMemory extends InfiniteMemory<ProgramRegister> {
 
+	/**
+	 * Constructor of the Program Memory. It reads all the lines in the program file and
+	 * charges them in the Array List of ProgramRegister.
+	 * @param programFilename String that contains the name of the program file.
+	 * @throws IOException
+	 */
 	public ProgramMemory(String programFilename) throws IOException {
 		super();
 		
@@ -24,11 +33,17 @@ public class ProgramMemory extends InfiniteMemory<ProgramRegister> {
 		reader.close();
 	}
 
+	/* (non-Javadoc)
+	 * @see daa.practice1.randomaccessmachine.memory.InfiniteMemory#setRegisterAt(int, java.lang.Object)
+	 */
 	@Override
 	public void setRegisterAt(int index, ProgramRegister data) {
 		memory.set(getPosition(index), data);
 	}
 
+	/* (non-Javadoc)
+	 * @see daa.practice1.randomaccessmachine.memory.InfiniteMemory#getRegisterAt(int)
+	 */
 	@Override
 	public ProgramRegister getRegisterAt(int index) {
 		if (index < 0 || index >= memory.size()) {
