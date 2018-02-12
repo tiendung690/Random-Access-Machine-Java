567a28f1d49763c58f6017c71ec6b3c816dc376b
Angel Igareta <alu0100967111@ull.edu.es>
2018-02-12 15:26:19 -0500

    InputTape commented.

--- src/daa/practice1/randomaccessmachine/io/InputTape.java
+++ src/daa/practice1/randomaccessmachine/io/InputTape.java
@@ -1,28 +1,46 @@
-/**
- * 
- */
+/** File InputTape, */
 package daa.practice1.randomaccessmachine.io;
 
+/** Import the IOPackage necessary for FileReader and BufferedReader. */
 import java.io.*;
 
+
 /**
- * @author angel
- *
+ * Class that does all the necessary operations with the inputTape. This includes
+ * creating the buffer, reading from it and closing it.
+ * @author Ángel Igareta
+ * @version 1.0
+ * @since 12-2-2018
  */
 public class InputTape {
 
+	/** InputTape is a Buffer of the inputTape. */
 	private BufferedReader inputTape;
 	
-	public InputTape(String outputTapeFilename) throws FileNotFoundException {
-		inputTape = new BufferedReader(new FileReader(outputTapeFilename));
+	/**
+	 * Constructor of the InputTape class. It initializes the inputTape to a BufferedReader
+	 * that contains text from a character-input stream.
+	 * @param outputTapeFilename Name of the file that contains the inputTape.
+	 * @throws FileNotFoundException
+	 */
+	public InputTape(String inputTapeFilename) throws FileNotFoundException {
+		inputTape = new BufferedReader(new FileReader(inputTapeFilename));
 	}
 	
+	/**
+	 * Read a line from the buffer that contains text from the inputTape.
+	 * @return String read in the file that contains the inputTape.
+	 * @throws IOException
+	 */
 	public String read() throws IOException {
 		return inputTape.readLine();
 	}
 	
+	/**
+	 * Close the buffer where inputTape was reading.
+	 * @throws IOException
+	 */
 	public void close() throws IOException {
 		inputTape.close();
 	}
-
 }
