d73907015128a3f93f818aa0d3c7483172203cf0
Angel Igareta <alu0100967111@ull.edu.es>
2018-02-19 17:36:03 -0500

    Finished documentation of the ArithmeticLogicUnit.

--- src/daa/practice1/randomaccessmachine/alu/ArithmeticLogicUnit.java
+++ src/daa/practice1/randomaccessmachine/alu/ArithmeticLogicUnit.java
@@ -1,66 +1,136 @@
+/** File ArithmeticLogicUnit */
 package daa.practice1.randomaccessmachine.alu;
 
 import daa.practice1.randomaccessmachine.memory.register.DataRegister;
 
 /**
- * [DESCRIPTION]
+ * Abstract class that represents the Arithmetic Logic Unit of the Random Access
+ * Machine. It has all the necessary methods to assign, add, subtract, multiply
+ * and divide registers with another registers or with constants.
+ * 
  * @author Ángel Igareta
  * @version 1.0
- * @since X-X-2018
+ * @since 16-2-2018
  */
 public abstract class ArithmeticLogicUnit {
-	
+
+	/*
+	 * Method used to assign a value to a register passed by argument.
+	 * 
+	 * @param register1 
+	 * @param value
+	 */
 	public static void assign(DataRegister register1, Integer value) {
 		register1.set(value);
 	}
-	
+
+	/**
+	 * Method used to add the values of two registers and save the result in the
+	 * first register passed by argument.
+	 * 
+	 * @param register1
+	 * @param register2
+	 */
 	public static void add(DataRegister register1, DataRegister register2) {
 		int sum = register1.get() + register2.get();
 		register1.set(sum);
 	}
-	
+
+	/**
+	 * Method used to add the values of a register and a constant, saving the result
+	 * in the first register passed by argument.
+	 * 
+	 * @param register1
+	 * @param constant
+	 */
 	public static void add(DataRegister register1, int constant) {
 		int sum = register1.get() + constant;
 		register1.set(sum);
 	}
-	
-	public static void sub(DataRegister register1, DataRegister register2) {
+
+	/**
+	 * Method used to subtract the values of two registers and save the result in
+	 * the first register passed by argument.
+	 * 
+	 * @param register1
+	 * @param register2
+	 */
+	public static void subtract(DataRegister register1, DataRegister register2) {
 		int sub = register1.get() - register2.get();
 		register1.set(sub);
 	}
-	
-	public static void sub(DataRegister register1, int constant) {
+
+	/**
+	 * Method used to subtract the values of a register and a constant, saving the
+	 * result in the first register passed by argument.
+	 * 
+	 * @param register1
+	 * @param constant
+	 */
+	public static void subtract(DataRegister register1, int constant) {
 		int sub = register1.get() - constant;
 		register1.set(sub);
 	}
-	
-	public static void mul(DataRegister register1, DataRegister register2) {
+
+	/**
+	 * Method used to multiply the values of two registers and save the result in
+	 * the first register passed by argument.
+	 * 
+	 * @param register1
+	 * @param register2
+	 */
+	public static void multiply(DataRegister register1, DataRegister register2) {
 		int mul = register1.get() * register2.get();
 		register1.set(mul);
 	}
-	
-	public static void mul(DataRegister register1, int constant) {
+
+	/**
+	 * Method used to multiply the values of a register and a constant, saving the
+	 * result in the first register passed by argument.
+	 * 
+	 * @param register1
+	 * @param constant
+	 */
+	public static void multiply(DataRegister register1, int constant) {
 		int mul = register1.get() * constant;
 		register1.set(mul);
 	}
-	
-	public static void div(DataRegister register1, DataRegister register2) throws ArithmeticException {
+
+	/**
+	 * Method used to divide the values of two registers and save the result in the
+	 * first register passed by argument.
+	 * 
+	 * @param register1
+	 * @param register2
+	 * @throws ArithmeticException
+	 *           If the denominator of the division is 0.
+	 */
+	public static void divide(DataRegister register1, DataRegister register2) throws ArithmeticException {
 		if (register2.get() == 0) {
 			throw new ArithmeticException("The denominator of the division can't be 0.");
 		}
 		else {
 			int div = register1.get() / register2.get();
-			register1.set(div);			
+			register1.set(div);
 		}
 	}
-	
-	public static void div(DataRegister register1, int constant) throws ArithmeticException {
+
+	/**
+	 * Method used to divide the values of a register and a constant, saving the
+	 * result in the first register passed by argument.
+	 * 
+	 * @param register1
+	 * @param constant
+	 * @throws ArithmeticException
+	 *           If the denominator of the division is 0.
+	 */
+	public static void divide(DataRegister register1, int constant) throws ArithmeticException {
 		if (constant == 0) {
 			throw new ArithmeticException("The denominator of the division can't be 0.");
 		}
 		else {
 			int div = register1.get() / constant;
-			register1.set(div);			
+			register1.set(div);
 		}
 	}
 
