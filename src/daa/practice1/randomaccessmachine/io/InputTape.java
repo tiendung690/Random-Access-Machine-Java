cd40441bea9e344ecdfdeb9fb3462d0d341cea26
Angel Igareta <alu0100967111@ull.edu.es>
2018-02-19 17:27:00 -0500

    Finished documentation of IO module.

--- src/daa/practice1/randomaccessmachine/io/InputTape.java
+++ src/daa/practice1/randomaccessmachine/io/InputTape.java
@@ -4,54 +4,64 @@ package daa.practice1.randomaccessmachine.io;
 /** Import the IOPackage necessary for FileReader and BufferedReader. */
 import java.io.*;
 
-
 /**
- * Class that does all the necessary operations with the inputTape. This includes
- * creating the buffer, reading from it and closing it.
+ * Class that does all the necessary operations with the inputTape. This
+ * includes creating the buffer, reading from it and closing it.
+ * 
  * @author Ángel Igareta
  * @version 1.0
  * @since 12-2-2018
  */
 public class InputTape {
 
-	/** InputTape is a Buffer of the inputTape. */
+	/** inputTape is a Buffer of the Input File. */
 	private BufferedReader inputTape;
-	
+
 	/**
-	 * Constructor of the InputTape class. It initializes the inputTape to a BufferedReader
-	 * that contains text from a character-input stream.
-	 * @param inputTapeFilename Name of the file that contains the inputTape.
-	 * @throws FileNotFoundException
+	 * Constructor of the InputTape class. It initializes the inputTape to a
+	 * BufferedReader that contains text from a character-input stream.
+	 * 
+	 * @param inputTapeFilename
+	 *          Name of the file that contains the inputTape.
+	 * @throws IOException
+	 *           If the file is not found.
 	 */
-	public InputTape(String inputTapeFilename) throws FileNotFoundException {
+	public InputTape(String inputTapeFilename) throws IOException {
 		inputTape = new BufferedReader(new FileReader(inputTapeFilename));
 	}
-	
+
 	/**
-	 * Read a line from the buffer that contains text from the inputTape.
+	 * Read a line from the buffer that contains text from the inputTape. This line
+	 * must be an integer. If it isn't read() throws an error.
+	 * 
 	 * @return String read in the file that contains the inputTape.
-	 * @throws IOException
+	 * @throws Exception
+	 *           Throws an error if the line is not an integer or there was an error
+	 *           reading it.
 	 */
-	public Integer read() throws Exception {
+	public Integer read() throws IOException {
 		String lineRead;
 		int returnValue;
-		
+
 		try {
 			lineRead = inputTape.readLine();
 			returnValue = Integer.parseInt(lineRead);
 		}
 		catch (IOException ioException) {
-			throw new Exception("There was a problem reading the number.");
+			throw new IOException("There was a problem reading the number.");
 		}
-		catch (NumberFormatException nfException) {
-			throw new Exception("The read value is not a number.");
+		catch (NumberFormatException numberException) {
+			throw new IOException("The read value is not a number.");
 		}
+
 		return returnValue;
 	}
-	
+
 	/**
 	 * Close the buffer where inputTape was reading.
+	 * 
 	 * @throws IOException
+	 *           Throws an error if there was a problem closing the buffer.
 	 */
 	public void close() throws IOException {
 		inputTape.close();
--- src/daa/practice1/randomaccessmachine/io/OutputTape.java
+++ src/daa/practice1/randomaccessmachine/io/OutputTape.java
@@ -4,42 +4,50 @@ package daa.practice1.randomaccessmachine.io;
 /** Import the IOPackage necessary for FileWriter and BufferedWriter. */
 import java.io.*;
 
-
 /**
- * Class that does all the necessary operations with the outputTape. This includes
- * creating the buffer, writing in it and closing it.
+ * Class that does all the necessary operations with the outputTape. This
+ * includes creating the buffer, writing in it and closing it.
+ * 
  * @author Ángel Igareta
  * @version 1.0
  * @since 12-2-2018
  */
 public class OutputTape {
-	
-	/** OutputTape is a Buffer of the outputTape. */
+
+	/** outputTape is a Buffer of the Output File. */
 	private BufferedWriter outputTape;
 
 	/**
-	 * Constructor of the OutputTape class. It initializes the outputTape to a BufferedWriter
-	 * that can write text to a character-output stream.
-	 * @param outputTapeFilename Name of the file that contains the outputTape.
-	 * @throws FileNotFoundException
+	 * Constructor of the OutputTape class. It initializes the outputTape to a
+	 * BufferedWriter that can write text to a character-output stream.
+	 * 
+	 * @param outputTapeFilename
+	 *          Name of the file that contains the outputTape.
+	 * @throws IOException
+	 *           Throws an error if the file was not found.
 	 */
 	public OutputTape(String outputTapeFilename) throws IOException {
 		outputTape = new BufferedWriter(new FileWriter(outputTapeFilename));
 	}
-	
+
 	/**
 	 * Writes a line to the buffer that contains text from the outputTape.
-	 * @param numberToWrite String that contains the line that is going to be written.
+	 * 
+	 * @param numberToWrite
+	 *          String that contains the line that is going to be written.
 	 * @throws IOException
+	 *           Throws an error if there was a problem writing the number.
 	 */
 	public void write(int numberToWrite) throws IOException {
 		outputTape.write(String.valueOf(numberToWrite));
 		outputTape.newLine();
 	}
-	
+
 	/**
-	 * Close the buffer where input was writing.
+	 * Close the buffer where the input was writing.
+	 * 
 	 * @throws IOException
+	 *           Throws an error if there was a problem closing the buffer.
 	 */
 	public void close() throws IOException {
 		outputTape.close();
